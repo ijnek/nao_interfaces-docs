e1c85d5639eef47135dd96e7ed4e1c6ee1b0d447
Kenji Brameld <kenjibrameld@gmail.com>
2021-05-15 17:21:04 +0900

    add an icon

--- conf.py
+++ conf.py
@@ -97,6 +97,8 @@ html_static_path = ['_static']
 #
 # html_sidebars = {}
 
+html_logo = 'images/nao_interfaces_icon.png'
+
 
 # -- Options for HTMLHelp output ---------------------------------------------
 
